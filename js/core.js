48c52a02458abaab1d594cac2c9ffd7863206f41
Herdian N <11958843+grittyninja@users.noreply.github.com>
2015-05-28 22:30:34 +0700

    modified

--- js/core.js
+++ js/core.js
@@ -1,8 +1,7 @@
-// Array 4D
 var Map = 	[
 				null,
 				["H1",
-					[["Q1",0.34],["Q3",0.28],["Q4",0.21],["Q2",0.17]]],
+					[["Q3",0.28],["Q4",0.21],["Q2",0.17]]],
 				["H2",
 					[["Q6",0.42],["Q5",0.33],["Q7",0.17],["Q8",0.08]]],
 				["H3",
@@ -16,10 +15,30 @@ var Map = 	[
 				["H7",
 					[["Q5",0.56],["Q6",0.44]]],
 				["H8",
-					[["Q20",0.45],["Q4",0.34],["Q10",0.18]]]
+					[["Q4",0.34],["Q10",0.18]]]
 		]
-// Array 4D
+
 var Pivot = Map
+function rmPivotQ(Q){
+	for(i=1;i<Pivot.length;i+=1){
+		temp = Pivot[i][1]
+		for(j=0;j<temp.length;j+=1){
+			if(Q==Pivot[i][1][j][0]){
+				if(j==0){ 
+					Pivot[i][1].shift()
+				}
+				else{
+					for(l=0;l<Pivot[i][1].length;l+=1){
+						if(temp[l][0]==Q){
+							Pivot[i][1].splice(l, 1)
+						}
+					}
+					
+				}
+			}
+		}
+	}
+}
 function getMinQ(arrQ){
 	temp = []
 	for(i=1;i<Pivot.length;i+=1){
@@ -75,7 +94,6 @@ function getQ(){
 	temp = null
 	if(arrQ[0] in Rule){
 		temp = arrQ[0]
-		arrQ.shift()
 		return temp
 	}
 	else{
@@ -86,19 +104,43 @@ function getQ(){
 		else{
 			temp = arrTemp
 		}
-		// Remove Q
-		if(temp==arrQ[0]) arrQ.shift()
-		else{rmArrQ(temp)} 
-		console.log(temp)
-		return temp
+		return temp.toString()
 	}
 }
 
 function rmArrQ(Q){
-	index = arrQ.indexOf(Q);
-	arrQ.splice(index, 1);
+	if(Q==arrQ[0])arrQ.shift()
+	else{
+		index = arrQ.indexOf(Q)
+		arrQ.splice(index, 1)
+	}
+	rmPivotQ(Q)
+	
 }
 
+function inferensi(){
+	if(arrH.length==1){
+		console.log(arrH)
+	}
+	else if(arrQ.length==0){
+		arrH = getHWmax()
+		console.log(arrH)
+	}
+
+}
+
+function getHWmax(){
+	maks = []
+	ans_ = null
+	Object.keys(Wt).forEach(function(key) {
+		maks.push(Wt[key])
+	});
+	maxx  = Math.max.apply(Math, maks);
+	Object.keys(Wt).forEach(function(key) {
+		if(Wt[key]==maxx)ans_ = key
+	});
+	return ans_
+}
 function checkEigen(Q){
 	temp = []
 	for(h=0;h<Q.length;h+=1){
@@ -111,33 +153,75 @@ function checkEigen(Q){
 			}
 		}
 	}
-	max  = Math.max.apply(Math, temp);
-	index = temp.indexOf(max)
+	maxz  = Math.max.apply(Math, temp);
+	index = temp.indexOf(maxz)
 	return Q[index]
 }
+function weight(H_,Q_){
+	for(il=0;il<H_.length;il+=1){
+		Wt[H_[il]] += getEigen(H_[il],Q_)
+	}
+}
 
-function isTrue(Q){
-	if(Q in Rule){
-		Con = Rule[Q]
-		arrQ = null
-		arrH = Con
+function getEigen(_H,_Q){
+	eigen = 0
+	for(i=1;i<Pivot.length;i+=1){
+		if(_H==Pivot[i][0]){
+			for(j=0;j<Pivot[i][1].length;j+=1){
+				if(_Q==Pivot[i][1][j][0]){
+					return Pivot[i][1][j][1]
+				}
+			}
+		}
+	}
+}
+function isTrue(Q__){
+	if(Q__ in Rule){
+		Con = Rule[Q__]
+		arrQ = []
+		arrH = [Con]
 		return null
 	}
 	else{
 		if(arrH == "start"){
-			arrH = getH(Q)
-			weight(arrH,Q) // Progress
-			arrQ = 
+			arrH = getH(Q__)
+			weight(arrH,Q__)
+			unQ = difference(arrQ,getTargetQ(arrH))
+			unlinkQ(unQ)
+			arrQ = getTargetQ(arrH)
 		}
 		else{
-			arrTemp = getH(Q)
+			arrTemp = getH(Q__)
 			arrTemp = isect(arrTemp,arrH)
-			weight(arrH,Q)
+			weight(arrTemp,Q__)
 		}
 
 	}
 }
 
+function difference(a1, a2) {
+  var result = [];
+  for (var i = 0; i < a1.length; i++) {
+    if (a2.indexOf(a1[i]) === -1) {
+      result.push(a1[i]);
+    }
+  }
+  return result;
+}
+
+function unlinkQ(Qiu){
+	for(ko=0;ko<Qiu.length;ko+=1){
+		rmPivotQ(Qiu[ko])
+	}
+}
+
+function getTargetQ(Ha){
+	qq = []
+	for(bl=0;bl<Ha.length;bl+=1){
+		qq = arrUnique(qq,getQD(Ha[bl]))
+	}
+	return qq
+}
 // Get H which have relation to Q
 function getH(Q){
 	Dep = []
@@ -165,4 +249,22 @@ function isect(alpha, beta) {
 		res.push(alpha[j]);
 	}
 	return res;
+}
+function arrUnique(a,b){
+	var c = a.concat(b.filter(function (item) {
+    	return a.indexOf(item) < 0;
+	}));
+	return c
+}
+function getQD(HH){
+	DeP = []
+	for(im=1;im<Pivot.length;im+=1){
+		if(Pivot[im][0]==HH){
+			temp = Pivot[im][1]
+			for(jim=0;jim<temp.length;jim+=1){
+				DeP.push(temp[jim][0])
+			}
+		}
+	}
+	return DeP
 }
